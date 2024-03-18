c00b093cc768cf55a3a1a82efcfea8e239462828
Kai Siang <izackscks@gmail.com>
2024-03-18 18:16:51 +0800

    + peer2peer editing

--- collaboration-service/app/src/App.jsx
+++ collaboration-service/app/src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef } from 'react'
 import Editor from "@monaco-Editor/react"
+import * as Y from "yjs"
+import {WebrtcProvider} from "y-webrtc"
+import {MonacoBinding} from "y-monaco"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const editorRef = useRef(null);
+
+  function handleEditorDidMount(editor, monaco){
+    editorRef.current = editor;
+
+    const doc = new Y.Doc();
+
+    const provider = new WebrtcProvider("test-space", doc);
+    const type = doc.getText("monaco");
+
+    const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness);
+    console.log(provider.awareness);
+  }
 
   return (
     <Editor
       height={"100vh"}
       width={"100vw"}
       theme="vs-dark"
-      
+      onMount={handleEditorDidMount}
       />
   )
 }
