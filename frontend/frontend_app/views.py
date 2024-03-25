c94f4e5236ae1119f046a970492ce3505c943303
Jac <e0895980@u.nus.edu>
2024-03-25 15:50:49 +0800

    added ability to create and delete questions

--- frontend/frontend_app/views.py
+++ frontend/frontend_app/views.py
@@ -7,6 +7,8 @@ def render_frontend(request):
     response = requests.get(QUESTION_SERVICE_ENDPOINT)
     if response.status_code == 200:
         data = response.json()
+        for item in data:
+            item['id'] = item['_id']
     else:
         print(f'Error occurred connecting to question-service API: {response.status_code}')
         print(response)
